5fdd308df21b47f33e0f79bb154def2e531c5fbe
Bill Blum <bill.blum@gmail.com>
2025-03-01 17:59:44 -0500

    OK, got a single input working.

--- TrackerAppProject/TrackerApp/TrackerApp.cs
+++ TrackerAppProject/TrackerApp/TrackerApp.cs
@@ -1,9 +1,38 @@
-﻿namespace TrackerApp;
+﻿using System;
+
+namespace TrackerApp;
 
 class Program
 {
+
+    static string GetUserInput(){
+        Console.WriteLine("Please enter emotion, or 'quit' to end:");
+        string userinput = Console.ReadLine() ?? string.Empty;
+        return userinput;
+    }
+
+    static bool ProcessUserInput(string userinput){
+        Console.WriteLine($"User entered: {userinput}.");
+
+        if( String.Equals(userinput, "quit", StringComparison.OrdinalIgnoreCase)){
+            return false;
+        } else {
+            return true;
+        }
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool still_running=true;
+
+        while(still_running){
+            // TODO
+            // - display prompt
+            // - get the input
+            string userinput = GetUserInput();
+            // - process the input
+            still_running = ProcessUserInput(userinput);
+            // - record relevant data in datastore ( handle within processUserInput ?)
+        }
+
     }
 }
