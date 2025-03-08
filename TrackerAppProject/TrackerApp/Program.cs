8e7c26e584e68bca95ae045b3970d9e6e5923f0b
Bill Blum <bill.blum@gmail.com>
2025-03-08 11:17:13 -0500

    Let's go this route instead.

---
+++ TrackerAppProject/TrackerApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class TrackerLauncher
+{
+    public static int Main(string[] args)
+    {
+        var app = new TrackerApp();
+        return app.Run(args);
+    }
+}
--- TrackerAppProject/TrackerApp/TrackerApp.cs
+++ TrackerAppProject/TrackerApp/TrackerApp.cs
@@ -1,68 +1,52 @@
-﻿using Spectre.Console;
+using Spectre.Console;
 using System;
-
-
-public static class TrackerLauncher
-{
-    public static int Main(string[] args)
-    {
-        var app = new TrackerApp();
-
-        return app.Run(args);
-    }
-}
+using System.Collections.Generic;
 
 class TrackerApp
 {
-    public int Run(string[] args)
+  public int Run(string[] args)
+  {
+
+
+    AnsiConsole.Clear();
+
+    // TODO: Look up how to pass debug flags in, wrap these messages 
+    int consoleWidth = AnsiConsole.Profile.Width;
+    AnsiConsole.MarkupLine($"[bold yellow]Current Console Width: {consoleWidth}[/]");
+    AnsiConsole.MarkupLine($"[bold yellow]Args Length: {args.Length}[/]");
+
+
+    AnsiConsole.Write(
+        new FigletText("Welcome!")
+            .Centered()
+            .Color(Color.Cyan1)
+    );
+
+    AnsiConsole.Write(
+        new Rule("[bold green]Let's Get Started[/]")
+            .Centered()
+            .RuleStyle("green")
+    );
+
+    RunMoodTracker();
+    return 0;
+  }
+
+  private void RunMoodTracker()
+  {
+    bool stillRunning = true;
+
+    while (stillRunning)
     {
-        AnsiConsole.Markup("[bold yellow]Hello World![/]");
-        AnsiConsole.WriteLine();
+      List<string> emotions = new() { "Happy", "Sad", "Mad", "Indifferent" };
+      string prompt = "Enter an emotion, or 'quit' to exit the app: ";
+      string userInput = UserInputHandler.GetUserInput(prompt, emotions, "Happy");
 
-        var name = AnsiConsole.Ask<string>("What's your [green]name[/]?");
-        AnsiConsole.MarkupLine($"[blue]Nice to meet you, {name}[/]");
-
-        return 0;
+      stillRunning = UserInputHandler.ProcessUserInput(userInput);
     }
+
+    // TODO: Add shutdown logic
+    // TODO: Stretch goal:  capture exception, ensure orderly shutdown?
+
+  }
 }
-
-// string GetUserInput(string prompt, List<string> options, string defaultOption)
-// {
-//     if (!options.Contains(defaultOption))
-//     {
-//         throw new ArgumentException("ERROR: Default option not provided.");
-//     }
-
-//     Console.Write(prompt);
-//     Console.Write($"[{defaultOption}]");
-
-//     string input = Console.ReadLine() ?? String.Empty;
-
-//     return (string.IsNullOrEmpty(input)) ? defaultOption : input;
-// }
-
-
-// bool ProcessUserInput(string userinput)
-// {
-//     Console.WriteLine($"User entered: {userinput}.");
-
-//     if (String.Equals(userinput, "quit", StringComparison.OrdinalIgnoreCase))
-//     {
-//         return false;
-//     }
-//     else
-//     {
-//         return true;
-//     }
-// }
-// bool still_running = true;
-
-// while (still_running)
-// {
-
-//     List<string> emotions = new List<string> { "Happy", "Sad", "Mad", "Indifferent" };
-//     string prompt = "Enter an emotion, or 'quit' to exit the app: ";
-//     string userinput = GetUserInput(prompt, emotions, "Happy");
-
-//     still_running = ProcessUserInput(userinput);
-// }
