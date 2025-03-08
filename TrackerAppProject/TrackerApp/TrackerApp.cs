bc5cb4d5119370bb546422113737c8f243d2f48f
Bill Blum <35750915+roadswitcher@users.noreply.github.com>
2025-03-08 17:42:53 -0500

    First attempt at setting up editor config and using dotnet format (#22)
    * Update editorconfig
    
    * First attempt, dotnet format.
    
    * Update README [skip ci]

--- TrackerAppProject/TrackerApp/TrackerApp.cs
+++ TrackerAppProject/TrackerApp/TrackerApp.cs
@@ -4,50 +4,49 @@ using System.Collections.Generic;
 
 class TrackerApp
 {
-  public int Run(string[] args)
-  {
+    public int Run(string[] args)
+    {
 
+        AnsiConsole.Clear();
 
-    AnsiConsole.Clear();
-
-    // TODO: Look up how to pass debug flags in, wrap these messages
+        // TODO: Look up how to pass debug flags in, wrap these messages
 #if DEBUG
-    int consoleWidth = AnsiConsole.Profile.Width;
-    AnsiConsole.MarkupLine($"[bold yellow]Current Console Width: {consoleWidth}[/]");
-    AnsiConsole.MarkupLine($"[bold yellow]Args Length: {args.Length}[/]");
+        int consoleWidth = AnsiConsole.Profile.Width;
+        AnsiConsole.MarkupLine($"[bold yellow]Current Console Width: {consoleWidth}[/]");
+        AnsiConsole.MarkupLine($"[bold yellow]Args Length: {args.Length}[/]");
 #endif
 
-    AnsiConsole.Write(
-        new FigletText("Welcome!")
-            .Centered()
-            .Color(Color.Cyan1)
-    );
+        AnsiConsole.Write(
+            new FigletText("Welcome!")
+                .Centered()
+                .Color(Color.Cyan1)
+        );
 
-    AnsiConsole.Write(
-        new Rule("[bold green]Let's Get Started[/]")
-            .Centered()
-            .RuleStyle("green")
-    );
+        AnsiConsole.Write(
+            new Rule("[bold green]Let's Get Started[/]")
+                .Centered()
+                .RuleStyle("green")
+        );
 
-    RunMoodTracker();
-    return 0;
-  }
-
-  private void RunMoodTracker()
-  {
-    bool stillRunning = true;
-
-    while (stillRunning)
-    {
-      List<string> emotions = new() { "Happy", "Sad", "Mad", "Indifferent" };
-      string prompt = "Enter an emotion, or 'quit' to exit the app: ";
-      string userInput = UserInputHandler.GetUserInput(prompt, emotions, "Happy");
-
-      stillRunning = UserInputHandler.ProcessUserInput(userInput);
+        RunMoodTracker();
+        return 0;
     }
 
-    // TODO: Add shutdown logic
-    // TODO: Stretch goal:  capture exception, ensure orderly shutdown?
+    private void RunMoodTracker()
+    {
+        bool stillRunning = true;
 
-  }
+        while (stillRunning)
+        {
+            List<string> emotions = new() { "Happy", "Sad", "Mad", "Indifferent" };
+            string prompt = "Enter an emotion, or 'quit' to exit the app: ";
+            string userInput = UserInputHandler.GetUserInput(prompt, emotions, "Happy");
+
+            stillRunning = UserInputHandler.ProcessUserInput(userInput);
+        }
+
+        // TODO: Add shutdown logic
+        // TODO: Stretch goal:  capture exception, ensure orderly shutdown?
+
+    }
 }
