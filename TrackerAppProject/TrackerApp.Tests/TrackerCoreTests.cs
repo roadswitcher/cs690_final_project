d364b212d71f1aece3db6ea4b8bbf388ce8236f3
Bill Blum <bill.blum@gmail.com>
2025-03-08 20:04:22 -0500

    That's not working, revisit tomorrow

--- TrackerAppProject/TrackerApp.Tests/TrackerCoreTests.cs
+++ TrackerAppProject/TrackerApp.Tests/TrackerCoreTests.cs
@@ -15,27 +15,3 @@ public class BasicPipelineDummyTest
     }
 }
 
-public class TrackerAppUserInputTests
-{
-
-    [Fact]
-    public void AskForMood_ShouldReturnUserInput_WhenSimulatingKeyPresses()
-    {
-        // Arrange: Create a TestConsole instance for simulating user input.
-        var console = new TestConsole().EmitAnsiSequences();
-
-        // \x1b[B = Down Arrow, \n = Enter key
-        console.Input.PushText("\x1b[B\n");
-
-        List<string> emotions = new() { "Happy", "Sad", "Mad", "Indifferent", "Quit" };
-
-        var handler = new UserInputHandler(console);
-
-        // Act: Get the user's input (simulated through the TestConsole)
-        var result = handler.GetUserInput(emotions);
-
-        // Assert: Verify that the selected mood is "Sad"
-        Assert.Equal("Sad", result);  // We simulate Down Arrow to select "Sad"
-    }
-
-}
