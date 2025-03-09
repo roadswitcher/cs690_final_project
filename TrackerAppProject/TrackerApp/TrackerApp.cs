aef7b9f6fd57acec128411ae329b5f7e482ed634
Bill Blum <bill.blum@gmail.com>
2025-03-09 16:30:42 -0400

    Saving state.

--- TrackerAppProject/TrackerApp/TrackerApp.cs
+++ TrackerAppProject/TrackerApp/TrackerApp.cs
@@ -1,11 +1,23 @@
 ﻿using Spectre.Console;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 
 class TrackerApp
 {
     private readonly IAnsiConsole _console;
     private readonly UserInputHandler _userInputHandler;
+    public class MoodRecord
+    {
+        public string Mood { get; }
+        public string? Trigger { get; }
+
+        public MoodRecord(string mood, string? trigger = null)
+        {
+            Mood = mood;
+            Trigger = trigger;
+        }
+    }
 
     public TrackerApp(IAnsiConsole console)
     {
@@ -21,17 +33,37 @@ class TrackerApp
         return 0;
     }
 
+    public void DebugPrint(string message)
+    {
+#if DEBUG
+        Console.WriteLine(message);
+#else
+        // Nothing happens in Release mode
+#endif
+    }
+
     private void RunMoodTracker()
     {
         bool stillRunning = true;
 
+
         while (stillRunning)
         {
-            List<string> emotions = new() { "Happy", "Sad", "Mad", "Indifferent", "Quit" };
+            List<string> trackedEmotions = new() { "Happy", "Sad", "Mad", "Indifferent" };
 
-            string userInput = _userInputHandler.GetUserInput(emotions);
+            var userInput = _userInputHandler.GetUserInput(trackedEmotions);
 
-            stillRunning = UserInputHandler.ProcessUserInput(userInput);
+            if (userInput is string choice)
+            {
+                DebugPrint($"User picked: {choice}");
+                stillRunning = !string.Equals(choice, "Exit", StringComparison.OrdinalIgnoreCase);
+            }
+            else if (userInput is MoodRecord mood)
+            {
+                // TODO: Update/store user mood
+            }
+
+            // stillRunning = UserInputHandler.ProcessUserInput(userInput);
         }
     }
 }
--- TrackerAppProject/TrackerApp/UserInputHandler.cs
+++ TrackerAppProject/TrackerApp/UserInputHandler.cs
@@ -1,6 +1,7 @@
 using Spectre.Console;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 public class UserInputHandler
 {
@@ -11,15 +12,31 @@ public class UserInputHandler
         _console = console ?? throw new ArgumentNullException(nameof(console));
     }
 
-    public string GetUserInput(List<string> options)
+    public object GetUserInput(List<string> options)
     {
-        return _console.Prompt(
+        // var choice = = _console.Prompt(
+        //     new SelectionPrompt<string>()
+        //         .Title("Update your emotion from the following list[red], or select 'Report' or 'Exit'.[/]")
+        //         .PageSize(20)
+        //         .MoreChoicesText("[grey]Use up/down arrows for more choices[/]")
+        //         .AddChoices(options)
+        // );
+        var choice = _console.Prompt(
             new SelectionPrompt<string>()
-                .Title("Update your emotion from the following list[red], or select 'Report' or 'Exit'.[/]")
-                .PageSize(20)
-                .MoreChoicesText("[grey]Use up/down arrows for more choices[/]")
-                .AddChoices(options)
+                .Title("[green]Update[/] mood, [aqua]Report[/] data, or [red]Exit[/] app?")
+                .AddChoices("[green]Update[/]", "[aqua]Report[/]", "[red]Exit[/]")
         );
+
+        // switch (choice)
+        // {
+        //     case "Exit", "Report":
+        //         return choice;
+
+        //     case "Update":
+        //         var response = GetMoodUpdate
+        // }
+
+        return Markup.Remove(choice);
     }
 
     public static bool ProcessUserInput(string userInput)
