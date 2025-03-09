c3706db295ce57dfe56e82ac8c81310f80b7466d
Bill Blum <bill.blum@gmail.com>
2025-03-08 19:56:19 -0500

    Buy a C# dev a beverage, learm more about dependency injection.

--- TrackerAppProject/TrackerApp/Program.cs
+++ TrackerAppProject/TrackerApp/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using Spectre.Console;
 
 public static class TrackerLauncher
 {
     public static int Main(string[] args)
     {
-        var app = new TrackerApp();
+        var app = new TrackerApp(AnsiConsole.Console);
         return app.Run(args);
     }
 }
