9d302f48afc85211761a3346435b17f7b16ae8e4
Bill Blum <bill.blum@gmail.com>
2025-03-16 13:58:52 -0400

    start mocking out a datastore

--- TrackerAppProject/TrackerApp/DataStore.cs
+++ TrackerAppProject/TrackerApp/DataStore.cs
@@ -1,7 +1,24 @@
 namespace TrackerApp
 {
+    public class AppData
+    {
+        public UserCreds UserCredentials { get; set; } = new UserCreds();
+        public List<MoodRecord> MoodRecords { get; set; } = new List<MoodRecord>();
+    }
+
+    public class UserCreds
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+    
     public class DataStore
     {
+        private static DataStore _instance;
+        private static readonly object _lock = new object();
+        private string _databaseFilePath;
+        private List<MoodRecord> _moodRecords;
+        private UserCreds _userCreds;
         
     }
 }
