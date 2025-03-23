9b5a6bba427a3c2a6065bc63d934833756d2cbc2
Bill Blum <bill.blum@gmail.com>
2025-03-23 12:42:44 -0400

    Added initial test for weekly report count.

--- TrackerAppProject/TrackerApp/ReportHandler.cs
+++ TrackerAppProject/TrackerApp/ReportHandler.cs
@@ -5,6 +5,12 @@ namespace TrackerApp
         public DateTime Date { get; set; }
         public int TotalRecords { get; set; }
     }
+
+    public class WeeklyReport
+    {
+        public DateTime Date { get; set; }
+        public int TotalRecords { get; set; }
+    }
     public class ReportHandler(IDataStore dataStore)
     {
         // Give me a link to the DataStore singleton, or an exception if you cannot
@@ -22,6 +28,14 @@ namespace TrackerApp
 
             var report = new DailyReport { Date = date, TotalRecords = records.Count };
             
+            return report;
+        }
+
+        public WeeklyReport GetWeeklyReport(DateTime endDate)
+        {
+            var startDate = endDate.Date.AddDays(-7);
+            var records = _dataStore.GetMoodRecords().Where(record => record.Timestamp.Date == DateTime.Now.Date).ToList();
+            var report = new WeeklyReport { Date = DateTime.Now, TotalRecords = records.Count };
             return report;
         }
 
