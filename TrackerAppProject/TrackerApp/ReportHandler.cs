7da718b2e87a47d1421cf50f3660966ffde3db15
Bill Blum <bill.blum@gmail.com>
2025-03-23 12:17:08 -0400

    Starting in on report generation.

--- TrackerAppProject/TrackerApp/ReportHandler.cs
+++ TrackerAppProject/TrackerApp/ReportHandler.cs
@@ -1,5 +1,10 @@
 namespace TrackerApp
 {
+    public class DailyReport
+    {
+        public DateTime Date { get; set; }
+        public int TotalRecords { get; set; }
+    }
     public class ReportHandler(IDataStore dataStore)
     {
         // Give me a link to the DataStore singleton, or an exception if you cannot
@@ -10,5 +15,17 @@ namespace TrackerApp
         // - Weekly Report ( aggregate, time of day )
         // Stretch goal- Trends Identification?
 
+        public DailyReport GetDailyReport(DateTime date)
+        {
+            date = date.Date;
+            var records = _dataStore.GetMoodRecords().Where(record => record.Timestamp.Date == date).ToList();
+
+            var report = new DailyReport { Date = date, TotalRecords = records.Count };
+            
+            return report;
+        }
+
     }
+
+
 }
