adfdaee198cd9a0c63b8fdc7a33145e98682eed0
Bill Blum <bill.blum@gmail.com>
2025-04-05 16:45:53 -0400

    Added a README

---
+++ TrackerAppProject/Utils/Build_And_Copy_Binary.ps1
@@ -0,0 +1,20 @@
+# On a Windows platform?   Run this, it should work.
+
+# If your Windows install is configured to block Powershell executation by default,
+# the following command MIGHT enable temporary execution of Powershell scripts in the
+# current Powershell session ( otherwise, just copy the binary manually )
+
+# Build a single file binary for Windows
+dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true /p:UseAppHost=true
+ 
+# Define the default path to the publish directory
+$publishDir = ".\TrackerApp\bin\Release\net8.0\win-x64\publish"
+
+# Construct the path to the single-file binary
+$binaryPath = Join-Path $publishDir "TrackerApp.exe"
+
+# Copy the binary to the current working directory
+Copy-Item $binaryPath $PWD
+
+Write-Host "Single-file binary 'TrackerApp.exe' has been copied to the current working directory."
+
